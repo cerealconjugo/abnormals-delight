c2fdbc86d758cac9af41db718a5476ded6fdc476
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-29 15:22:56 -0500

    Fix an inconsistency

--- src/main/java/com/teamabnormals/abnormals_delight/core/other/ADTiers.java
+++ src/main/java/com/teamabnormals/abnormals_delight/core/other/ADTiers.java
@@ -6,6 +6,6 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public class ADTiers {
-	public static final Tier SILVER = new BlueprintItemTier(0, 111, 7.0F, 0.0F, 17, () -> Ingredient.of(ADItemTags.INGOTS_SILVER));
-	public static final Tier NECROMIUM = new BlueprintItemTier(4, 2251, 9.0F, 3.0F, 12, () -> Ingredient.of(ADItemTags.INGOTS_NECROMIUM));
+	public static final Tier SILVER = new BlueprintItemTier(1, 157, 9.0F, 1.0F, 18, () -> Ingredient.of(ADItemTags.INGOTS_SILVER));
+	public static final Tier NECROMIUM = new BlueprintItemTier(4, 2031, 9.0F, 3.0F, 15, () -> Ingredient.of(ADItemTags.INGOTS_NECROMIUM));
 }
